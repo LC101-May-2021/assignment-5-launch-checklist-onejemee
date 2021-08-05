1bf64d601dfc3933c7f81e9e131b7285e4a6fc74
onejemee <84356486+onejemee@users.noreply.github.com>
2021-08-05 14:41:49 -0500

    completed Assignment 5

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-// Write your JavaScript code here!
+// const { pickPlanet, addDestinationInfo } = require("./scriptHelper");
+//const {formSubmission} = require("./scriptHelper")
 
 window.addEventListener("load", function() {
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let listedPlanet = pickPlanet(listedPlanets)
+        console.log(listedPlanet)
+        addDestinationInfo(document, listedPlanet.name, listedPlanet.diameter, listedPlanet.star, listedPlanet.distance, listedPlanet.moons, listedPlanet.image)
+    
    })
-   
+let listStatus = document.getElementById("faultyItems")
+let form = this.document.querySelector("form")  
+form.addEventListener("submit", function(event) {
+        event.preventDefault();
+        formSubmission(document, listStatus, pilotName, copilotName, fuelLevel, cargoMass);
+    });
 });
