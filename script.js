fdcf24d37061036e9dfb41946226b466dc87d3c5
onejemee <84356486+onejemee@users.noreply.github.com>
2021-08-06 18:23:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,14 @@ listStatus.style.visibility = "hidden"
 let form = this.document.querySelector("form")  
 form.addEventListener("submit", function(event) {
         event.preventDefault();
+        let pilot = document.querySelector("input[name=pilotName]")
+        let pilotName = pilot.value
+        let copilot = document.querySelector("input[name=copilotName]")
+        let copilotName = copilot.value
+        let fuel = document.querySelector("input[name=fuelLevel]")
+        let fuelLevel = fuel.value
+        let cargo = document.querySelector("input[name=cargoMass]")
+        let cargoMass = cargo.value
         formSubmission(document, listStatus, pilotName, copilotName, fuelLevel, cargoMass);
     });
 });
